ad8a484f4fd3e593073717cdee3fdc96fdb34497
aledark <konachan101@gmail.com>
2017-12-17 16:37:14 -0500

    Imagenes pokimons batalla

--- app/Http/Controllers/BatallaController.php
+++ app/Http/Controllers/BatallaController.php
@@ -3,13 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\User;
+use App\Pokemon;
+use App\Entrenador;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Auth;
 
 class BatallaController extends Controller
 {
     public function index(){
-        return View('Batallas.index');
+
+    	$pokemons = Pokemon::entrenadorPokemon(Auth::User()->idEntrenador)->get();
+    	$urlsImagenes = array();
+    	foreach ($pokemons as $pokemon) {
+            array_push($urlsImagenes, "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/".$pokemon->numeroPokemon.".png");
+        }
+        return View('Batallas.index')->with('imagenes',$urlsImagenes);
     }
 }
