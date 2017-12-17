0d7c4cd74b06ac25b6f268d0aafa09545170368f
Daniel Rivera <danriveraz@hotmail.com>
2017-12-17 16:30:21 -0500

    cambios para aleji

--- resources/views/Batallas/index.blade.php
+++ resources/views/Batallas/index.blade.php
@@ -1,4 +1,30 @@
 @extends('Layout.app')
 @section('content')
-
+	<div class="cotainer">
+		<div id="divPiedra" class="">
+			<img id="piedra" src="{{asset('img/piedra.png')}}" alt="piedra" width="15%">
+		</div>
+		<div id="divPapel" class="">
+			<img id="papel" src="{{asset('img/papel.png')}}" alt="papel" width="15%">
+		</div>
+		<div id="divTijeras" class="">
+			<img id="tijeras" src="{{asset('img/tijeras.png')}}" alt="tijeras" width="15%">
+		</div>
+	</div>
+	<div class="container">
+		<div class="form-group" id="myPokemons">
+    			<img width="20%" src="{{$imagenes[0]}}" alt="Portada"
+    			id="pok1">
+	    		<img width="20%" src="{{$imagenes[1]}}" alt="Portada"
+    			id="pok2">
+	    		<img width="20%" src="{{$imagenes[2]}}" alt="Portada"
+    			id="pok3">
+	    		<img width="20%" src="{{$imagenes[3]}}" alt="Portada"
+    			id="pok4">
+	    		<img width="20%" src="{{$imagenes[4]}}" alt="Portada"
+    			id="pok5">
+				<img width="20%" src="{{$imagenes[5]}}" alt="Portada"
+    			id="pok6">
+    		</div>
+	</div>
 @endsection
---
+++ resources/views/Batallas/perfil.blade.php
@@ -0,0 +1,148 @@
+@extends('Layout.app')
+@section('content')
+<div class="container-fluid" align="center">
+	<div id="form-content" class="col-sm-6">
+		<div id="encabezado" class="form-group">
+	    	<img id="portada" src="\ProyectoWWW\public\img\perfiles\{{$entrenador->imagenPerfil}}" alt="Portada">
+	    </div>
+    	<form action="{{url('entrenador/editar')}}" class="formulario" id="form-perfil" method="POST" enctype="multipart/form-data" >
+    		{{csrf_field()}}
+    		<div id="imgPerfil" class="glyphicon glyphicon-camera" align="center">
+			    <input id="imagenPerfil" type="file" name="imagenPerfil">
+			</div>
+			<br>
+    		<div class="form-group">
+    			<h3 id="nombre">{{$nickname}}</h3>
+    		</div>
+    		<div class="form-group"	>
+    			<input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre" value="{{$entrenador->nombre}}" 
+    			minlength="5" maxlength="20">
+    		</div>
+    		<div class="form-group">
+    			<input type="text" class="form-control" name="edad" id="edad" placeholder="Edad" value="{{$entrenador->edad}}" min="5">
+    		</div>
+    		<div class="form-group" >
+    			<select class="form-control" name="sexo" id="sexo" style="text-align:center;">
+    				<option> Sexo </option>
+    				<option id="femenino" value="Femenino"
+    				 <?php if($entrenador->sexo == "Femenino") echo "selected=''" ?> > Femenino </option>
+    				<option id="masculino" value="Masculino"
+    				<?php if($entrenador->sexo == "Masculino") echo "selected=''" ?> > Masculino </option>
+    			</select> 
+    		</div>
+    		<div class="form-group">
+    			<input type="text" class="form-control" name="pais" id="pais" placeholder="Pais" value="{{$entrenador->pais}}">
+    		</div>	
+    		<div class="form-group">
+    			<input type="password" class="form-control" name="password" id="password" placeholder="Contraseña" value="{{$entrenador->password}}" hidden="" min="4" max="15">
+    		</div>
+    		<div class="form-group">
+    			<button style="background-color: transparent !important; border:none;" class="btn btn-primary"><span id="pen-save" class="glyphicon glyphicon-pencil"> </span></button>
+		    		
+		    	</a>	
+    		</div>
+    	</form>
+    </div>
+    <div class="col-sm-6">
+    	<h1 id="title-equipo">	Tu equipo Pokémon</h1>
+    	<br>
+    	<div class="container">
+    		<div class="form-group" id="myPokemons">
+    			<img width="20%" src="{{$imagenes[0]}}" alt="Portada"
+    			id="pok1" data-html="true"   <?php echo 'title="'. ucwords($pokemon[0]->nombre) .'<br>Habilidades"';?>  			 
+	    			data-content="
+	    			<div>
+	    			<strong>
+	    				<li>{{ucwords($habilidades[0])}}<br></li>
+	    				<li>{{ucwords($habilidades[1])}}<br></li>
+	    				<li>{{ucwords($habilidades[2])}}<br></li>
+	    				<li>{{ucwords($habilidades[3])}}<br></li>
+	    			</strong>
+	    			</div>"
+	    			data-placement="bottom" data-toggle="popover">
+	    		<img width="20%" src="{{$imagenes[1]}}" alt="Portada"
+    			id="pok2" data-html="true" <?php echo 'title="'. ucwords($pokemon[1]->nombre) .'<br>Habilidades"';?>
+	    			data-content="
+	    			<div>
+	    			<strong>
+	    				<li>{{ucwords($habilidades[4])}}<br></li>
+	    				<li>{{ucwords($habilidades[5])}}<br></li>
+	    				<li>{{ucwords($habilidades[6])}}<br></li>
+	    				<li>{{ucwords($habilidades[7])}}<br></li>
+	    			</strong>
+	    			</div>"
+	    			data-placement="bottom" data-toggle="popover">
+	    		<img width="20%" src="{{$imagenes[2]}}" alt="Portada"
+    			id="pok3" data-html="true" <?php echo 'title="'. ucwords($pokemon[2]->nombre) .'<br>Habilidades"';?>
+	    			data-content="
+	    			<div>
+	    			<strong>
+	    				<li>{{ucwords($habilidades[8])}}<br></li>
+	    				<li>{{ucwords($habilidades[9])}}<br></li>
+	    				<li>{{ucwords($habilidades[10])}}<br></li>
+	    				<li>{{ucwords($habilidades[11])}}<br></li>
+	    			</strong>
+	    			</div>"
+	    			data-placement="bottom" data-toggle="popover">
+	    		<img width="20%" src="{{$imagenes[3]}}" alt="Portada"
+    			id="pok4" data-html="true" <?php echo 'title="'. ucwords($pokemon[3]->nombre) .'<br>Habilidades"';?>
+	    			data-content="
+	    			<div>
+	    			<strong>
+	    				<li>{{ucwords($habilidades[12])}}<br></li>
+	    				<li>{{ucwords($habilidades[13])}}<br></li>
+	    				<li>{{ucwords($habilidades[14])}}<br></li>
+	    				<li>{{ucwords($habilidades[15])}}<br></li>
+	    			</strong>
+	    			</div>"
+	    			data-placement="bottom" data-toggle="popover">
+	    		<img width="20%" src="{{$imagenes[4]}}" alt="Portada"
+    			id="pok5" data-html="true" <?php echo 'title="'. ucwords($pokemon[4]->nombre) .'<br>Habilidades"';?>
+	    			data-content="
+	    			<div>
+	    			<strong>
+	    				<li>{{ucwords($habilidades[16])}}<br></li>
+	    				<li>{{ucwords($habilidades[17])}}<br></li>
+	    				<li>{{ucwords($habilidades[18])}}<br></li>
+	    				<li>{{ucwords($habilidades[19])}}<br></li>
+	    			</strong>
+	    			</div>"
+	    			data-placement="bottom" data-toggle="popover">
+				<img width="20%" src="{{$imagenes[5]}}" alt="Portada"
+    			id="pok6" data-html="true" <?php echo 'title="'. ucwords($pokemon[5]->nombre) .'<br>Habilidades"';?>
+	    			data-content="
+	    			<div>
+	    			<strong>
+	    				<li>{{ucwords($habilidades[20])}}<br></li>
+	    				<li>{{ucwords($habilidades[21])}}<br></li>
+	    				<li>{{ucwords($habilidades[22])}}<br></li>
+	    				<li>{{ucwords($habilidades[23])}}<br></li>
+	    			</strong>
+	    			</div>"
+	    			data-placement="bottom" data-toggle="popover">
+    		</div>
+    	</div>
+    </div>
+</div>
+<br>
+<div class="container" id="batallas-table" align="center">
+	<h1>Historial de batallas</h1>
+	<br>
+	<table class="table table-bordered table-striped" id="historial">
+		<thead>
+			<th> Nombre oponente </th>
+			<th> Resultado </th>
+			<th> Fecha </th>
+		</thead>
+		<tbody>
+			@foreach($batallas as $batalla)
+				<tr>
+					<td>{{$batalla->entrenadorArtificial->nickname}}</td>
+					<td>{{$batalla->resultado}}</td>
+					<td>{{$batalla->fecha}}</td>
+				</tr>
+			 @endforeach
+		</tbody>
+	</table>
+</div>
+@endsection
