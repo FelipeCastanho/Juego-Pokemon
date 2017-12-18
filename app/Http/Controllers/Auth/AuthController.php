404690367373bff3aa7db7c553898f65eff5be0c
aledark <konachan101@gmail.com>
2017-12-18 00:27:25 -0500

    Optimización registro

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -80,10 +80,17 @@ class AuthController extends Controller
     }
 
     public function pokemonTeam($idEntrenador){
-        $numbers = range(1, 386);
-        shuffle($numbers);
-        $pokemons = array_slice($numbers, 0, 5);
+
+        $x = 0;
+        $pokemons = array();
         array_push($pokemons, 25);
+        while ($x<5) {
+          $num_aleatorio = rand(1,386);
+          if (!in_array($num_aleatorio,$pokemons)) {
+            array_push($pokemons,$num_aleatorio);
+            $x++;
+          }
+        }
 
         foreach ($pokemons as $pokemon) {
             $this->pokemonHabilities($pokemon,$idEntrenador);
